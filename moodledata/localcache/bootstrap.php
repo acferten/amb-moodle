37d82c73256956f68358cf13e5bb48e609f9243d
acferten <gowiththiername@gmail.com>
2023-03-29 01:36:46 +0700

    idea removed

---
+++ moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'oHTpzCcMhKYhjPPYKhcBI4UOKfp0hp7Ulocalhost';
+$CFG->bootstraphash = '5ec7b8d755161f8401e5eeb98c2c73bc';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
